1e1f00f013d40d170831aa1235f3f4fc57da3423
Adrian Suliga <adriansuliga81@gmail.con>
2025-03-19 09:26:17 +0100

    Solve lab3

---
+++ Lab3/reverse.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int sum_file(char *input)
+{
+    int file = open(input, O_RDONLY);
+    char buffor;
+    int sum = 0;
+
+    while (read(file, &buffor, sizeof(buffor))) {
+        sum += (int)buffor;
+    }
+
+    return sum;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3) {
+        printf("Expected 2 arguments, but got %d", argc - 1);
+        return 0;
+    }
+
+    char *input_path = argv[1];
+    char *output_path = argv[2];
+
+    int start_offset = -1;
+    int control_sum = sum_file(input_path);
+
+    int input_file = open(input_path, O_RDONLY);
+    int output_file = open(output_path, O_WRONLY);
+
+    while (lseek(input_file, start_offset, SEEK_END) >= 0) {
+        char buffer;
+        if (read(input_file, &buffer, sizeof(buffer)) <= 0) {
+            printf("Could not read from file\n");
+        } 
+        write(output_file, &buffer, sizeof(buffer));
+        start_offset--;
+    }
+
+    close(input_file);
+    close(output_file);
+
+    printf("Input check sum: %d\nOutput check sum: %d\n", control_sum, sum_file(output_path));
+
+    return 0;
+}
